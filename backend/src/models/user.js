8c01089057ef2404e0aa2390cc9c7ac2fc75edb3
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-12-29 13:30:15 +0530

    add authentication middleware

---
+++ backend/src/models/user.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true, select: false },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+    lastlogin: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const UserModel = new mongoose.model("users", userSchema);
+export default UserModel;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import cookieparser from "cookie-parser";
 dotenv.config();
 connectDb();
 
 const app = express();
+app.use(express.json());
+app.use(cookieparser());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcom to User Management Server");
