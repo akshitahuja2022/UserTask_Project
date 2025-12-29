3174f64fabc05b2d9131484e8b5114e42a4a8779
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-12-29 15:20:51 +0530

    add user authentication

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,122 @@
+import UserModel from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const signup = async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+
+    const existedUser = await UserModel.findOne({ email });
+    if (existedUser) {
+      return res.status(403).json({
+        message: "User already exist with this email",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await UserModel.create({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    const jwtToken = jwt.sign({ _id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("userToken", jwtToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: "Signup Successfully",
+      success: true,
+      user: {
+        fullname: user.fullname,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email }).select("+password");
+    if (!user) {
+      return res
+        .status(403)
+        .json({ message: "Invalid Credentials", success: false });
+    }
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return res
+        .status(401)
+        .json({ message: "Invalid Crendentials", success: false });
+    }
+
+    const jwtToken = jwt.sign(
+      {
+        _id: user.id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.cookie("userToken", jwtToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      message: "Login Successfully",
+      success: true,
+      user: {
+        fullname: user.fullname,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const logout = async (req, res) => {
+  try {
+    res.clearCookie("userToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ message: "Logout Successfully", success: true });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export { signup, login, logout };
