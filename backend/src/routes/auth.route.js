08817b47126769f7ee3e69d95b026d0377e95e26
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-12-29 15:48:10 +0530

    Add protected route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,14 +1,23 @@
 import express from "express";
 import {
   loginValidation,
+  ProtectedRoute,
   signupValidation,
 } from "../middleware/auth.middleware.js";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  isAuthenticate,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signupValidation, signup);
 authRouter.post("/login", loginValidation, login);
 authRouter.post("/logout", logout);
+
+// check user is authenticate or not
+authRouter.get("/authenticate", ProtectedRoute, isAuthenticate);
 
 export default authRouter;
