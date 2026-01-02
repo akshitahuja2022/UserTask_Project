4ed9c4b61afc6224f91b455b9782c9873e9a4553
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-01-02 22:13:44 +0530

    Handle Cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -29,7 +29,7 @@ const signup = async (req, res) => {
     });
     res.cookie("userToken", jwtToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV,
+      secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 24 * 60 * 60 * 1000,
     });
